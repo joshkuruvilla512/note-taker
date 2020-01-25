ebf848183a707f07ec2cccac0d927a447321a199
Joshua Kuruvilla <joshkuruvilla512@gmail.com>
2020-01-25 11:41:10 -0600

    Using the server.js from Hot Restaurant as a template for the server.js for this homework.

--- Develop/db/store.js
+++ Develop/db/store.js
@@ -1,1 +1,36 @@
 // WHERE WE STORE OUR NOTES
+
+const util = require("util");
+const fs = require("fs");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class STORE {constructor(){
+    this.lastID=0
+};
+
+read() {
+    return readFileAsync(./db/db.json)
+    //  UTF8
+}
+
+write(note) {
+
+}
+
+getNotes() {
+
+}
+
+addNotes(note) {
+
+}
+
+deleteNotes(id) {
+
+}
+
+};
+
+module.exports = new STORE();
